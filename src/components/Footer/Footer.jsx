81a6573cd61410eef894ce9c6cb631e46f977fc8
prathibha <prsthibha@gmail.com>
2022-10-09 10:40:05 +0530

    Add Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+94-7729-40633'>+94-7729-40633</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:prsthibha@gmail.com'>prsthibha@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialIcons href='https://github.com'>
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://linkedin.com'>
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://instagram.com'>
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+      </SocialContainer>
+    </FooterWrapper>
   );
 };
 
